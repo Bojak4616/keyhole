62dbf9c2b9725e495d1ecff768fac14d016e7d43
Jesse Buonanno <jpb4616@rit.edu>
2016-08-15 19:56:02 -0400

    Cleaned up crypto and padding issue

--- client/client.py
+++ client/client.py
@@ -2,7 +2,26 @@
 
 import requests
 import json
+import hashlib, binascii
+import os
 from requests.auth import HTTPBasicAuth
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+
+def pad(msg):
+    return msg + str((16 - (len(msg) % 16)) * '\0')
+
+def encrypt(key, string, iv):
+    crypto_suite = AES.new(pad(key), AES.MODE_CBC, iv)
+    cipher_text = crypto_suite.encrypt(pad(string))
+    return cipher_text.encode('hex')
+
+def decrypt(key, string, iv):
+    decrypt_suite = AES.new(pad(key), AES.MODE_CBC, iv)
+    string_de = string.decode('hex')
+    plain_text = decrypt_suite.decrypt(string_de)
+    print plain_text
 
 # create a new user
 def create_user():
@@ -36,12 +55,14 @@ def get_token():
 def create_record(user_token):
     url = 'http://127.0.0.1:5000/api/records/new'
     print '[+] Enter the values below to add a new record to the database...'
-
     ip = raw_input('[+] IP Address: ')
     uname = raw_input('[+] Username: ')
     key = raw_input('[+] SSH key: ')
     passw = raw_input('[+] Password: ')
     comments = raw_input('[+] Comments: ')
+    password = raw_input("[+] Enter password for crypto: ")
+    crypt_ip = encrypt(password, ip)
+    print crypt_ip
 
     response = requests.post(url, auth=HTTPBasicAuth(user_token, 'x'), json={'ip':ip, 'uname':uname, 'key':key, 'passw':passw, 'comments':comments})
 
@@ -57,6 +78,7 @@ def search_record(user_token):
 
 if __name__ == '__main__':
     while True:
+        print "create user, get token, create record, search record"
         uinput = raw_input("What would you like to do? ")
         if uinput == "create user":
             create_user()
@@ -67,5 +89,12 @@ if __name__ == '__main__':
             create_record(token)
         elif uinput == "search record":
             search_record(token)
+        elif uinput == "decrypt it":
+            decrypt(raw_input('passworrrd: '), raw_input('cipher: ').strip(), raw_input('iv: ').strip())
+        elif uinput == "test":
+            #key, string-to-crypt, iv
+            cipher_text = encrypt('bbbb', 'ccc', 'a'*16)
+            decrypt('bbbb', cipher_text, 'a'*16)
         else:
             print "blah you're wrong"
+
